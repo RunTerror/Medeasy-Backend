8e8cd6de4f3aec9c14143aae16edfd28189d9a2e
AdityaKulshrestha <kulshresthaaditya02@gmail.com>
2024-04-06 11:09:35 +0530

    Added chain for follow up questions

--- backend/doctor_agent.py
+++ backend/doctor_agent.py
@@ -8,33 +8,61 @@ from langchain.agents import initialize_agent
 
 from dotenv import load_dotenv
 
-
 load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 
 class Diagnoser:
     def __init__(self):
-        diagnose_prompt = """You are an expert medical expert. Your task it to convert the given hindi conversation into English. So that it becomes easier for the doctor to diagnose the disease.
+        diagnose_prompt = """You are a medical expert. Your task it to convert the given hindi conversation into English. So that it becomes easier for the doctor to diagnose the disease.
         {patient_complain}
         """
-        self.llm_template = PromptTemplate.from_template(diagnose_prompt)
-        self.llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY)
-        self.diganose_chain = LLMChain(llm=self.llm, prompt=self.llm_template, verbose=True)
-        # self.chain = self.llm_template | self.llm
+        validation_prompt = """You are a medical experiment. You have been given the patient's complain. You have to 
+        diagnose the patient's disease. Identify and ask crucial question to help you narrow down and diagnose the 
+        patient. Ask only one question based on the response. If no chat-history is provided ask initial first question. 
+         
+        Previous Conversation: {previous_conversation} 
 
+        Patient Complain:
+        {complain}
+
+        Question:
+        """
+        self.symptoms_template = PromptTemplate.from_template(diagnose_prompt)
+        self.ask_patient = PromptTemplate.from_template(validation_prompt)
+        self.llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY)
+        self.diganose_chain = LLMChain(llm=self.llm, prompt=self.symptoms_template, verbose=True)
+        self.ask_patient_chain = LLMChain(llm=self.llm, prompt=self.ask_patient, verbose=False)
+
+    #
     def describe_symptoms(self, complaint):
-        output = self.diganose_chain.run(patient_complain = complaint)
+        output = self.diganose_chain.run(patient_complain=complaint)
         return output
 
-    def initial_diagnose(self, ):
+    def ask_validation_questions(self, symptoms, chat_history={}):
+        conversation_history = self._process_chathistory(chat_history)
+        question = self.ask_patient_chain.run(complain=symptoms, previous_conversation=conversation_history)
+        return question
+
+    @staticmethod
+    def _process_chathistory(chathistory):
+        conversation_string = ""
+        for doctor_response, patient_response in chathistory.items():
+            conversation_string += f"Question: {doctor_response} \n Patient's Response: {patient_response}"
+
+        return conversation_string
 
 
 if __name__ == "__main__":
     diagnoser = Diagnoser()
-    complain = "Mujhe pet m bohot tez acidity ho rhi h aur saath m jee ghabra rha h"
-    initial_diagnose = diagnoser.initial_diagnosing_questionaire(complain)
-    print(initial_diagnose)
-
+    complain = "Mere pairon ke talvon par soojan aa rakhi h"
+    patient_symptoms = diagnoser.describe_symptoms(complain)
+    print(patient_symptoms)
+    chat_history = {}
+    for i in range(4):
+        follow_up_questions = diagnoser.ask_validation_questions(symptoms=patient_symptoms,chat_history=chat_history)
+        print(f"Doctor : {follow_up_questions}")
+        patient_response = input("Patient: ")
+        chat_history[follow_up_questions] = patient_response
 
 
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI, UploadFile, File
+from pydantic import BaseModel
+from doctor_agent import Diagnoser
 
 app = FastAPI()
+
+
+class PatientDetails(BaseModel):
+    age: int
+    heartbeat_rate: int
+    complaint: str
+    metadata: str
+
+
+@app.on_event("startup")
+
 
 
 @app.post("/upload/")
@@ -22,8 +35,26 @@ async def upload_reports(file1: UploadFile = File(...), file2: UploadFile = File
     file2_contents = await file2.read()
     file2_info = {
         "filename": file2.filename,
-        "content_type":file2.content_type,
+        "content_type": file2.content_type,
         "size": len(file2_contents)
     }
 
-    return
+    return {"file1": file1_info, "file2": file2_info}
+
+
+@app.post("/initial_diagnose")
+async def run_diagnosis(patient_info: PatientDetails):
+    """
+    Endpoint to accept patients' details and run a initial diagnosis
+    :param patient_info:
+    :return:
+    """
+
+def analyse_ECG(self):
+    """
+    Diagnose ECG data using custom trained model
+    :param self:
+    :return:
+        tuple: A tuple containing the predicted results and the test duration.
+    """
+    model_path =
