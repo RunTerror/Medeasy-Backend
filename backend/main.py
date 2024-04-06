14bed1e6097670a9c5f52875f284c7272c0637a0
Abhishek <bansalabhishek7411@gmail.com>
2024-04-06 13:36:36 +0530

    cors added

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware  # Import CORSMiddleware
 from pydantic import BaseModel
 from langchain_google_genai import ChatGoogleGenerativeAI
 from doctor_agent import Diagnoser
@@ -6,6 +7,14 @@ import uvicorn
 
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins, you can specify specific domains instead of "*"
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Specify allowed HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 class PatientDetails(BaseModel):
     age: int
@@ -14,55 +23,12 @@ class PatientDetails(BaseModel):
     metadata: str
     chat_history: dict
 
-
-# @app.on_event("startup")
-# @app.post("/upload/")
-# async def upload_reports(file1: UploadFile = File(...), file2: UploadFile = File(...)):
-#     """
-#
-#     :param file1:
-#     :param file2:
-#     :return:
-#     """
-#
-#     file1_contents = await file1.read()
-#     file1_info = {
-#         "filename": file1.name,
-#         "content_type": file1.content_type,
-#         "size": len(file1_contents)
-#     }
-#
-#     file2_contents = await file2.read()
-#     file2_info = {
-#         "filename": file2.filename,
-#         "content_type": file2.content_type,
-#         "size": len(file2_contents)
-#     }
-#
-#     return {"file1": file1_info, "file2": file2_info}
-
 @app.post("/initial_diagnose")
 async def run_diagnosis(patient_info: PatientDetails):
-    """
-    Endpoint to accept patients' details and run a initial diagnosis and ask follow up questions
-    :param patient_info:
-    :return:
-    """
     diagnoser = Diagnoser()
     follow_up_question = diagnoser.ask_validation_questions(symptoms=patient_info.complaint,
                                                             chat_history=patient_info.chat_history)
     return {'follow_question': follow_up_question}
-
-
-@app.post("/analyse_ecg")
-def analyse_ECG(self):
-    """
-    Diagnose ECG data using custom trained model
-    :param self:
-    :return:
-        tuple: A tuple containing the predicted results and the test duration.
-    """
-
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
